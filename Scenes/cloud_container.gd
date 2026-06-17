3f8d876d69512d5c8109b7c9d53653287e755676
egoia <elyas.saidoune@gmail.com>
2026-06-17 14:09:01 +0200

    started to light those clouds

---
+++ Scenes/cloud_container.gd
@@ -0,0 +1,14 @@
+@tool
+extends MeshInstance3D
+@export var light : Light3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_instance_shader_parameter("light_position", light.transform.origin)
+	NoiseTexture3D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
