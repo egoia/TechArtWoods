4b822a814c0b99165651d851da5b212d5134050a
egoia <elyas.saidoune@gmail.com>
2026-06-24 01:25:26 +0200

    started reworking light calculation

--- Scenes/cloud_container.gd
+++ Scenes/cloud_container.gd
@@ -1,14 +1,13 @@
 @tool
 extends MeshInstance3D
-@export var light : Light3D
+@export var light : DirectionalLight3D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_instance_shader_parameter("light_position", light.transform.origin)
-	NoiseTexture3D
+	set_instance_shader_parameter("light_direction", -light.transform.basis.z)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	set_instance_shader_parameter("light_direction", -light.transform.basis.z)
